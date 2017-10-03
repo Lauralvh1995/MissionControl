3c9ce6277b922898f7da4a5587a4527b9a44cdd2
Laura van Helden <l.vanhelden@student.fontys.nl>
2017-10-03 10:25:11 +0200

    added classes. Astronaut fully done.

---
+++ MissionControl/Astronaut.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MissionControl
+{
+    class Astronaut
+    {
+        string Name { get; }
+        string Gender { get; }
+        string Nationality { get; }
+
+        public Astronaut(string name, string gender, string nationality)
+        {
+            Name = name;
+            Gender = gender;
+            Nationality = nationality;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Gender + " " + Nationality;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
